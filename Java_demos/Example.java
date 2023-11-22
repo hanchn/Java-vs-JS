29338e49917770b69aec24ef5a48f3025692516b
hanchn <jing.yuan@kapeixi.com>
2023-11-22 15:51:03 +0800

    .

---
+++ Java_demos/Example.java
@@ -0,0 +1,15 @@
+/**
+ * Example
+ */
+public class Example {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    Example example = new Example();
+    example.print();
+  }
+
+  public void print() {
+    System.out.println("Print");
+  }
+}
