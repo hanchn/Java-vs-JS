5a90ab2be50c8740bb4ae17223235ebfa659d51b
hanchn <jing.yuan@kapeixi.com>
2023-11-22 14:32:28 +0800

    .

---
+++ Java_demos/Fun.java
@@ -0,0 +1,10 @@
+public class Fun {
+  public static void main(String[] args) {
+    String getTestValue = testFun();
+    System.out.println(getTestValue);
+  }
+
+  public static String testFun() {
+    return "test fun !";
+  }
+}
