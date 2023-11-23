ecb692e08c46a8dbb876e564e1a4ae4e7eb7e568
hanchn <jing.yuan@kapeixi.com>
2023-11-23 14:13:37 +0800

    .

--- JavaScript_demos/07.js
+++ JavaScript_demos/07.js
@@ -1,6 +1,18 @@
-class Test { 
-  
+class Person { 
+  #reg = /(\d{3})\d{4}(\d{4})/;
+  #phone = null
   constructor() { 
  
   }
+
+  get telNumber() { 
+    let phone = this.#phone;
+    phone = phone.replace(this.#reg, "$1****$2");
+    // 返回结果
+    return phone;
+  }
+
+  set telNumber(phone) { 
+    this.#phone = /^1[3-9]\\d{9}$/.test(phone) ? (typeof phone === 'string') ? phone : String(phone) : '你传入了非法手机号！';
+  }  
 }
