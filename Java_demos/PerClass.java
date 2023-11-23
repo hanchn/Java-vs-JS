7a7260f8aaa34f1fbb4d64c186b093e6b85d4ce1
hanchn <jing.yuan@kapeixi.com>
2023-11-23 11:14:00 +0800

    .

---
+++ Java_demos/PerClass.java
@@ -0,0 +1,9 @@
+public class PerClass {
+  public static void main(String[] args) {
+    System.out.println("PerClass");
+  }
+
+  public void print(String str) {
+    System.out.println(str);
+  }
+}
---
+++ Java_demos/SonClass.java
@@ -0,0 +1,8 @@
+public class SonClass extends PerClass {
+
+  public static void main(String[] args) {
+    System.out.println("SonClass");
+    SonClass sonClass = new SonClass();
+    sonClass.print("Hello World !");
+  }
+}
